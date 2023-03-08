253600decb9c36a49d2db61ce085ac7b5706c24b
Michał Dziekan <michulec7@gmail.com>
2023-03-09 00:33:28 +0100

    Registration GUI designing

--- modules/register.py
+++ modules/register.py
@@ -1,46 +1,45 @@
-
 from tkinter import *
 import customtkinter 
 
 
-customtkinter.set_appearance_mode("System")  
-customtkinter.set_default_color_theme("blue")  
+class Registration():
+
+    customtkinter.set_appearance_mode("System")  
+    customtkinter.set_default_color_theme("green")  
+
+    root=customtkinter.CTk()
+    root.geometry("500x350")
+    root.minsize(500,350)
+    root.title="BuddgetBuddy"
+
+    def Login():
+        print("Test")
+
+    frame=customtkinter.CTkFrame(master=root,width=1000, height=350)
+    frame.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+    frame.grid_rowconfigure(0,weight=1)
+    frame.grid_rowconfigure(1,weight=1)
+    frame.grid_rowconfigure(2,weight=1)
+    frame.grid_rowconfigure(3,weight=1)
+    frame.grid_rowconfigure(4,weight=1)
+    
+    frame.grid_columnconfigure((0,1,2), weight=1)
 
 
+    label = customtkinter.CTkLabel(master=frame, text="Budget Buddy", font=("Arial", 24 ))
+    label.grid(pady=12, padx=10,row=0, column=1)
 
-app = customtkinter.CTk()  # create CTk window 
-app.title("Budget Buddy")
-app.geometry("800x500")
-app.minsize(800, 500)
+    entery1=customtkinter.CTkEntry(master=frame, placeholder_text="Login")
+    entery1.grid(pady=12, padx=10,row=1, column=1)
+    
+    entery2=customtkinter.CTkEntry(master=frame, placeholder_text="Password",show="*")
+    entery2.grid(pady=12, padx=10, row=2, column=1)
 
-app.columnconfigure(0,weight=2)
-app.columnconfigure(1,weight=1)
+    button=customtkinter.CTkButton(master=frame, text="Login",command=Login)
+    button.grid(pady=12, padx=10, row=3, column=1)
 
-app.rowconfigure(0,weight=1)
-app.rowconfigure(1,weight=1)
-app.rowconfigure(2,weight=1)
-app.rowconfigure(3,weight=1)
-app.rowconfigure(4,weight=1)
+    checkbox=customtkinter.CTkCheckBox(master=frame, text="Remember me")
+    checkbox.grid(pady=12, padx=10, row=4, column=1)
 
-
-
-# tworzenie etykiety "Email"
-email_label = customtkinter.CTkLabel(master=app, text="E-mail",font=("Arial", 22))
-email_label.grid(column=0,row=0)
-
-# tworzenie pola do wprowadzania emaila
-email_entry = customtkinter.CTkEntry(master=app, placeholder_text="CTkEntry",corner_radius=8)
-email_entry.grid(column=0,row=1)
-
-# tworzenie etykiety "Hasło"
-password_label = customtkinter.CTkLabel(master=app, text="Hasło",font=("Arial", 22))
-password_label.grid(column=0,row=3)
-
-# tworzenie pola do wprowadzania hasła
-password_entry = customtkinter.CTkEntry(master=app, placeholder_text="CTkEntry",show="*",corner_radius=8)
-password_entry.grid(column=0,row=4)
-
-
-
-
-app.mainloop() #listening to events
+    root.mainloop()
