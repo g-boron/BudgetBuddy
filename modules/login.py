6a528db93724ec23b2c62d31daf44a6065e7ef09
m-szczepanski <matiszczep@gmail.com>
2023-03-10 13:05:13 +0100

    Update login.py

--- modules/login.py
+++ modules/login.py
@@ -10,6 +10,7 @@ class Login(customtkinter.CTk):
         super().__init__()
         self.geometry("800x600")
         self.title("Login in to your account")
+        icon_photo = PhotoImage(file="./images/budget_buddy_logo.png")
         self.frame = customtkinter.CTkFrame(master=self, width=800, height=600)
         self.frame.place(relx=0.5, rely=0.5, anchor=CENTER)
         self.frame.grid_rowconfigure(0, weight=1)
@@ -18,24 +19,25 @@ class Login(customtkinter.CTk):
         self.frame.grid_rowconfigure(3, weight=1)
         self.frame.grid_rowconfigure(4, weight=1)
         self.resizable(False, False)
-        #self.iconphoto() here is the place to put our logo
+
+        self.iconphoto(False, icon_photo)
         self.frame.grid_columnconfigure((0, 1, 2), weight=1)
 
         self.label = customtkinter.CTkLabel(master=self.frame, text="Enter your credentials",
                                             font=("Arial", 30, "normal"))
-        self.label.grid(pady=16, padx=10, row=0, column=1)
+        self.label.grid(pady=18, padx=10, row=0, column=1)
 
         self.entry1 = customtkinter.CTkEntry(master=self.frame, placeholder_text="Login", justify=CENTER)
-        self.entry1.grid(pady=16, padx=10, row=1, column=1, sticky="ew")
+        self.entry1.grid(pady=18, padx=10, row=1, column=1, sticky="ew")
 
         self.entry2 = customtkinter.CTkEntry(master=self.frame, placeholder_text="Password", show="*", justify=CENTER)
-        self.entry2.grid(pady=16, padx=10, row=2, column=1, sticky="ew")
+        self.entry2.grid(pady=18, padx=10, row=2, column=1, sticky="ew")
 
         self.button_login = customtkinter.CTkButton(master=self.frame, text="Login", command=self.login)
-        self.button_login.grid(pady=16, padx=10, row=3, column=1, sticky="ew")
+        self.button_login.grid(pady=18, padx=10, row=3, column=1, sticky="ew")
 
         self.checkbox = customtkinter.CTkCheckBox(master=self.frame, text="Remember me")
-        self.checkbox.grid(pady=16, padx=10, row=4, column=1)
+        self.checkbox.grid(pady=18, padx=10, row=4, column=1)
 
         self.button = customtkinter.CTkButton(master=self.frame, fg_color="transparent",
                                               text="Forgot your password?", font=("Arial", 12, "normal"),
