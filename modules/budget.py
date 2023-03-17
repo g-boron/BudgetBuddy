7df04183d5fb0841503ce87014e08f15c64fe75d
Grzegorz Boroń <grzesiekb2013@gmail.com>
2023-03-17 13:45:24 +0100

    Added checking if expense's amount is grater than user's balance

--- modules/budget.py
+++ modules/budget.py
@@ -6,14 +6,10 @@ class Budget:
     def __init__(self, username):
         self.username = username
         self.db = DatabaseConnector()
-        query = f"SELECT balance FROM users WHERE username = '{username}'"
+        query = f"SELECT balance FROM users WHERE username = '{self.username}'"
         self.balance = self.db.select_data(query, 'one')[0]
-        query = f"SELECT id FROM users WHERE username = '{username}'"
+        query = f"SELECT id FROM users WHERE username = '{self.username}'"
         self.user_id = self.db.select_data(query, 'one')[0]
-
-    
-    def get_balance(self):
-        return self.balance
 
 
     def add_expense(self, name, desc, amount, category):
@@ -22,9 +18,12 @@ class Budget:
         user = Budget('admin')
         user.add_expense('Testowy wydatek', 'Wydatek wprowadzony dla testow', 2500, 1)
         '''
-        query = f"INSERT INTO expenses (name, description, amount, add_date, user_id, category_id) VALUES ('{name}', '{desc}', '{amount}', '{date.today()}', '{self.user_id}', '{category}')"
-        self.db.make_query(query)
-        
-        current_balance = self.balance - amount
-        query = f"UPDATE users SET balance = {current_balance} WHERE username = '{self.username}'"
-        self.db.make_query(query)
+        if amount > self.balance:
+            return 'You do not have enough money!'
+        else:
+            query = f"INSERT INTO expenses (name, description, amount, add_date, user_id, category_id) VALUES ('{name}', '{desc}', '{amount}', '{date.today()}', '{self.user_id}', '{category}')"
+            self.db.make_query(query)
+            
+            current_balance = self.balance - amount
+            query = f"UPDATE users SET balance = {current_balance} WHERE username = '{self.username}'"
+            self.db.make_query(query)
