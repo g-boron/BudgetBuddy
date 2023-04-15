9b0b5e97b97d7097c1d297c2161ab96c6ede1303
Michał Dziekan <michulec7@gmail.com>
2023-04-16 01:09:49 +0200

    Password change
    Added option to change password

--- modules/change_password.py
+++ modules/change_password.py
@@ -4,6 +4,8 @@ import customtkinter
 from PIL import ImageTk
 from modules.database import database_connect
 import textwrap
+from tkinter import messagebox
+import psycopg2
 
 
 customtkinter.set_appearance_mode("System")
@@ -31,19 +33,53 @@ class ChangePassword(customtkinter.CTk):
                                             font=("Arial", 30, "normal"))
         self.label.grid(pady=18, padx=10, row=0, column=1)
 
-        self.login_entry = customtkinter.CTkEntry(master=self.frame, placeholder_text="New password", justify=CENTER)
-        self.login_entry.grid(pady=18, padx=10, row=1, column=1, sticky="ew")
+        self.password = customtkinter.CTkEntry(master=self.frame, placeholder_text="New password", show="*", justify=CENTER)
+        self.password.grid(pady=18, padx=10, row=1, column=1, sticky="ew")
 
-        self.password_entry = customtkinter.CTkEntry(master=self.frame, placeholder_text="Confirm new password", show="*",
+        self.password2 = customtkinter.CTkEntry(master=self.frame, placeholder_text="Confirm new password", show="*",
                                                      justify=CENTER)
-        self.password_entry.grid(pady=18, padx=10, row=2, column=1, sticky="ew")
+        self.password2.grid(pady=18, padx=10, row=2, column=1, sticky="ew")
 
-        self.password_entry = customtkinter.CTkEntry(master=self.frame, placeholder_text="Previous Password", show="*",
+        self.password_old = customtkinter.CTkEntry(master=self.frame, placeholder_text="Previous Password", show="*",
                                                      justify=CENTER)
-        self.password_entry.grid(pady=18, padx=10, row=3, column=1, sticky="ew")
+        self.password_old.grid(pady=18, padx=10, row=3, column=1, sticky="ew")
 
-        self.button_login = customtkinter.CTkButton(master=self.frame, text="Save changes")
-        self.button_login.grid(pady=18, padx=10, row=4, column=1, sticky="ew")
+        self.button_save = customtkinter.CTkButton(master=self.frame, text="Save changes",command=self.save_changes)
+        self.button_save.grid(pady=18, padx=10, row=4, column=1, sticky="ew")
 
        
 
+    def save_changes(self):
+        password_old = self.password_old.get()
+        password = self.password.get()
+        password2 = self.password2.get()
+
+        if password != password2:
+                messagebox.showerror("New passwords don't match", "Please make sure the passwords match.")
+                return False
+        
+        if len(password) < 8 or not any(char.isupper() for char in password) or \
+                    not any(char.islower() for char in password) or not any(char.isdigit() for char in password):
+                messagebox.showerror("Invalid password", "Password should be at least 8 characters long and contain at "
+                                                         "least one uppercase letter, one lowercase letter, and "
+                                                         "one digit.")
+                return False
+        
+
+        db = database_connect.DatabaseConnector()
+
+        query = f"SELECT password FROM users WHERE username = '{self.username}' " \
+                f"AND password = crypt('{password_old}', password);"
+        user_pass = db.select_data(query, 'one')
+
+        if user_pass is None:
+            messagebox.showerror("Invalid password", "Old password incorrect")
+            return False 
+
+        if user_pass is not None:
+            messagebox.showerror("Success", "Password has been changed.")
+
+            query_update = f"UPDATE users SET password = crypt('{password}', gen_salt('bf')) WHERE username = '{self.username}';"       
+            result = db.make_query(query_update)
+
+            return False 
