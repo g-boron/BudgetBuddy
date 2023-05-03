12b58544054c9f1b0cb89da55053173e40f14ab3
m-szczepanski <matiszczep@gmail.com>
2023-05-03 13:15:39 +0200

    added month expenses summary

--- modules/home_window.py
+++ modules/home_window.py
@@ -1,3 +1,4 @@
+import datetime
 from tkinter import *
 from tkinter import ttk
 import customtkinter
@@ -7,6 +8,7 @@ from modules.all_expenses import AllExpenses
 from modules.all_revenues import AllRevenues
 from modules.change_password import ChangePassword
 from modules.day_summary import DaySummary
+from modules.month_summary import MonthSummary
 from tkcalendar import Calendar
 import os
 from modules import login
@@ -59,15 +61,16 @@ class HomeWindow(customtkinter.CTk):
                                                 font=("Arial", 26, "normal"))
         self.element3.grid(pady=18, padx=10, row=5, column=0, sticky="new")
         self.change = customtkinter.CTkButton(master=self.menu_frame, text="Change Password", fg_color="transparent",
-                                                command=self.change_password, font=("Arial", 26, "normal"))
+                                              command=self.change_password, font=("Arial", 26, "normal"))
         
         self.change.grid(pady=18, padx=10, row=6, column=0, sticky="new")
         self.logout = customtkinter.CTkButton(master=self.menu_frame, text="Log out", fg_color="transparent",
-                                                command=self.logout, font=("Arial", 26, "normal"))
+                                              command=self.logout, font=("Arial", 26, "normal"))
         
         self.logout.grid(pady=18, padx=10, row=7, column=0, sticky="new")
 
-        self.calendar_frame = customtkinter.CTkFrame(master=self, width=(screen_width / 3), height=450, fg_color='#242424')
+        self.calendar_frame = customtkinter.CTkFrame(master=self, width=int(screen_width / 3), height=450,
+                                                     fg_color='#242424')
         self.calendar_frame.grid(column=0, row=2, sticky="n", rowspan=2)
         self.calendar_frame.grid_columnconfigure(0, weight=1)
         self.calendar_frame.grid_rowconfigure(0, weight=1)
@@ -89,7 +92,8 @@ class HomeWindow(customtkinter.CTk):
         self.description2.grid(pady=18, padx=10, row=1, column=1)
 
         db = database_connect.DatabaseConnector()
-        query = f"SELECT e.amount, c.name from expenses AS e JOIN users AS u ON e.user_id=u.id JOIN categories AS c ON e.category_id=c.id WHERE u.username='{self.username}'AND e.add_date=CURRENT_DATE"
+        query = f"SELECT e.amount, c.name from expenses AS e JOIN users AS u ON e.user_id=u.id JOIN categories AS " \
+                f"c ON e.category_id=c.id WHERE u.username='{self.username}'AND e.add_date=CURRENT_DATE"
         self.results = db.select_data(query)
 
         self.summary = {'Entertainment': 0, 'Shopping': 0, 'Bills': 0, 'Subscriptions': 0, 'Other': 0}
@@ -108,18 +112,50 @@ class HomeWindow(customtkinter.CTk):
         query = f"SELECT currency FROM users WHERE username='{self.username}'"
         self.currency = db.select_data(query, 'one')[0]
 
-
-        self.today_spending_frame = customtkinter.CTkFrame(master=self, width=int((screen_width / 3)),
-                                                                     height=270)
-        self.today_spending_frame.grid(column=1, row=2, sticky="ns")
-        self.today_spending_frame.grid_columnconfigure((0, 1), weight=1)
-        self.today_spending_frame.grid_rowconfigure((1, 2), weight=1)
-        self.total = customtkinter.CTkLabel(master=self.today_spending_frame, text=f"Daily total: {str(sum(self.summary.values()))} {self.currency}",
-                                                   font=("Arial", 30, "normal"))
+        self.spending_summary = customtkinter.CTkFrame(master=self, width=int((screen_width / 3)),
+                                                       height=270, fg_color="#242424")
+        self.spending_summary.grid(column=1, row=2, sticky="ns")
+        self.spending_summary.grid_columnconfigure((0, 1), weight=1)
+        self.spending_summary.grid_rowconfigure((0, 1), weight=1)
+        self.total = customtkinter.CTkLabel(master=self.spending_summary,
+                                            text=f"Daily total: {str(sum(self.summary.values()))} {self.currency}",
+                                            font=("Arial", 30, "normal"))
         self.total.grid(pady=18, padx=10, column=0, row=0)
 
-        self.view = customtkinter.CTkButton(master=self.today_spending_frame, text='View details', command=self.see_details, font=('Arial', 30, 'normal'))
+        self.view = customtkinter.CTkButton(master=self.spending_summary, text='View details',
+                                            command=self.see_details, font=('Arial', 30, 'normal'))
         self.view.grid(pady=18, padx=10, column=1, row=0)
+
+        current_month = datetime.datetime.now().month
+        current_year = datetime.datetime.now().year
+        query = f"SELECT e.amount, c.name, EXTRACT(MONTH FROM add_date) from expenses AS e JOIN " \
+                f"users AS u ON e.user_id=u.id JOIN categories AS c ON e.category_id=c.id " \
+                f"WHERE u.username='{self.username}' AND EXTRACT(MONTH FROM add_date) = {current_month} " \
+                f"AND EXTRACT(YEAR FROM add_date) = {current_year}"
+        self.month_results = db.select_data(query)
+        self.month_summary = {'Entertainment': 0, 'Shopping': 0, 'Bills': 0, 'Subscriptions': 0, 'Other': 0}
+        for r in self.month_results:
+            if r[1] == 'Entertainment':
+                self.month_summary['Entertainment'] += float(r[0])
+            elif r[1] == 'Shopping':
+                self.month_summary['Shopping'] += float(r[0])
+            elif r[1] == 'Bills':
+                self.month_summary['Bills'] += float(r[0])
+            elif r[1] == 'Subscriptions':
+                self.month_summary['Subscriptions'] += float(r[0])
+            else:
+                self.month_summary['Other'] += float(r[0])
+
+        query = f"SELECT currency FROM users WHERE username='{self.username}'"
+        self.currency = db.select_data(query, 'one')[0]
+        self.month_total = customtkinter.CTkLabel(master=self.spending_summary,
+                                                  text=f"Month total: {str(sum(self.month_summary.values()))} "
+                                                       f"{self.currency}", font=("Arial", 30, "normal"))
+        self.month_total.grid(pady=18, padx=10, column=0, row=1)
+
+        self.month_view = customtkinter.CTkButton(master=self.spending_summary, text='View details',
+                                            command=self.see_month_details, font=('Arial', 30, 'normal'))
+        self.month_view.grid(pady=18, padx=10, column=1, row=1)
 
         self.incoming_transactions_frame = customtkinter.CTkFrame(master=self, width=int((screen_width / 3)),
                                                                   height=160, fg_color="purple")
@@ -150,6 +186,10 @@ class HomeWindow(customtkinter.CTk):
     def see_details(self):
         day_summary = DaySummary(self.username, self.summary, self.currency, len(self.results))
         day_summary.mainloop()
+
+    def see_month_details(self):
+        month_summary = MonthSummary(self.username, self.month_summary, self.currency, len(self.month_results))
+        month_summary.mainloop()
 
     def get_user_name(self, user_login):
         db = database_connect.DatabaseConnector()
